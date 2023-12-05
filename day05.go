cc5815cf588d3db926ca813136684b9d87ab54bb
rumkugel13 <rumkugel13@outlook.com>
2023-12-05 18:16:12 +0100

    cleanup day05

--- day05.go
+++ day05.go
@@ -23,11 +23,10 @@ func day05() {
 	var smallest = math.MaxInt32
 	for _, seed := range seeds {
 		var num = seed
-		for i := 0; i < len(maps); i++ {
-			var rangeMaps = maps[i]
-			for _, m := range rangeMaps {
-				if num >= m.source && num <= m.source+m.length {
-					num = (num - m.source) + m.destination
+		for _, rangeMaps := range maps {
+			for _, rangeMap := range rangeMaps {
+				if num >= rangeMap.source && num <= rangeMap.source+rangeMap.length {
+					num = (num - rangeMap.source) + rangeMap.destination
 					break
 				}
 			}
@@ -38,40 +37,31 @@ func day05() {
 	var result = smallest
 	fmt.Println("Day 05 Part 1 Result: ", result)
 
-	var seedRanges = seedsToRangeArray(seeds)
 	var smallestRange = math.MaxInt32
-	for _, seedRange := range seedRanges {
+	for i := 0; i < len(seeds); i += 2 {
+		var seedRange = Range{0, seeds[i], seeds[i+1]}
 		var sourceRanges = []Range{seedRange}
-		
 		for _, rangeMaps := range maps {
-			var mappedRanges []Range
 			for _, rangeMap := range rangeMaps {
 				var length = len(sourceRanges)
 				for s := 0; s < length; s++ {
-					var r = sourceRanges[s]
-					if o, overlap := overlaps(r, rangeMap); o {
-						var split = splitRange(r, overlap)
-						if split[0].length > 0 {
-							sourceRanges = append(sourceRanges, split[0])
-						}
-						if split[2].length > 0 {
-							sourceRanges = append(sourceRanges, split[2])
-						}
-						var mapped = split[1]
+					var sourceRange = sourceRanges[s]
+					if itDoes, overlap := overlapsRange(sourceRange, rangeMap); itDoes {
+						var split = splitRange(sourceRange, overlap)
+						var mapped = split[0]
 						mapped.source = (mapped.source - rangeMap.source) + rangeMap.destination
-						mappedRanges = append(mappedRanges, mapped)
-						sourceRanges[s] = Range{}
-					} 
+						sourceRanges[s] = mapped
+						for j := 1; j <= 2; j++ {
+							if split[j].length > 0 {
+								sourceRanges = append(sourceRanges, split[j])
+							}
+						}
+					}
 				}
 			}
-			mappedRanges = append(mappedRanges, sourceRanges...)
-			clear(sourceRanges)
-			sourceRanges = append(sourceRanges, mappedRanges...)
 		}
-		for _,sources := range sourceRanges {
-			if sources.length > 0 {
-				smallestRange = min(smallestRange, sources.source)
-			}
+		for _, sources := range sourceRanges {
+			smallestRange = min(smallestRange, sources.source)
 		}
 	}
 
@@ -81,29 +71,20 @@ func day05() {
 
 func splitRange(seedRange, overlap Range) []Range {
 	var ranges []Range
-	ranges = append(ranges, Range{0, min(seedRange.source, overlap.source), abs(overlap.source-seedRange.source)})
 	ranges = append(ranges, overlap)
-	ranges = append(ranges, Range{0, min(seedRange.source+seedRange.length, overlap.source+overlap.length), 
-		abs((overlap.source + overlap.length)-(seedRange.source +seedRange.length))})
+	ranges = append(ranges, Range{0, min(seedRange.source, overlap.source), abs(overlap.source - seedRange.source)})
+	ranges = append(ranges, Range{0, min(seedRange.source+seedRange.length, overlap.source+overlap.length),
+		abs((overlap.source + overlap.length) - (seedRange.source + seedRange.length))})
 	return ranges
 }
 
-func overlaps(seedRange, mapRange Range) (bool, Range) {
-	left1, right1 := seedRange.source, seedRange.source + seedRange.length
-	left2, right2 := mapRange.source, mapRange.source + mapRange.length
+func overlapsRange(seedRange, mapRange Range) (bool, Range) {
+	left1, right1 := seedRange.source, seedRange.source+seedRange.length
+	left2, right2 := mapRange.source, mapRange.source+mapRange.length
 	if left1 <= right2 && right1 >= left2 {
 		return true, Range{0, max(left1, left2), min(right1, right2) - max(left1, left2)}
 	}
 	return false, Range{}
-}
-
-func seedsToRangeArray(seeds []int) RangeMap {
-	var ranges RangeMap
-	for i := 0; i < len(seeds); i += 2 {
-		var r = Range{seeds[i], seeds[i], seeds[i+1]}
-		ranges = append(ranges, r)
-	}
-	return ranges
 }
 
 func parseRangeMaps(lines []string) RangeMaps {
@@ -113,12 +94,10 @@ func parseRangeMaps(lines []string) RangeMaps {
 		if lines[i] == "" {
 			section++
 			maps = append(maps, RangeMap{})
-			i += 2
-			if i >= len(lines) {
-				break
-			}
+			i++
+		} else {
+			maps[section] = append(maps[section], parseRange(lines[i]))
 		}
-		maps[section] = append(maps[section], parseRange(lines[i]))
 	}
 	return maps
 }
