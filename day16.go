d5ca05d12c6f56deb912d5f08da80d8901b57214
rumkugel13 <rumkugel13@outlook.com>
2023-12-16 14:52:05 +0100

    day16: another refactor

--- day16.go
+++ day16.go
@@ -8,51 +8,39 @@ func day16() {
 	grid := getLines("input/16.txt")
 	pos := Point{-1, 0}
 	dir := Point{1, 0}
-	energized := map[Point]int{}
 
-	walkBeam(grid, pos, dir, energized)
-	var result = len(energized)
+	var result = walkBeam(grid, pos, dir)
 	fmt.Println("Day 16 Part 1 Result: ", result)
 
-	maxEnergized := len(energized)
-	clear(energized)
+	maxEnergized := 0
 	for row := range grid {
-		walkBeam(grid, Point{-1, row}, Point{1, 0}, energized)
-		maxEnergized = max(maxEnergized, len(energized))
-		clear(energized)
-
-		walkBeam(grid, Point{len(grid[0]), row}, Point{-1, 0}, energized)
-		maxEnergized = max(maxEnergized, len(energized))
-		clear(energized)
+		maxEnergized = max(maxEnergized, walkBeam(grid, Point{-1, row}, Point{1, 0}))
+		maxEnergized = max(maxEnergized, walkBeam(grid, Point{len(grid[0]), row}, Point{-1, 0}))
 	}
 
 	for col := range grid[0] {
-		walkBeam(grid, Point{col, -1}, Point{0, 1}, energized)
-		maxEnergized = max(maxEnergized, len(energized))
-		clear(energized)
-
-		walkBeam(grid, Point{col, len(grid)}, Point{0, -1}, energized)
-		maxEnergized = max(maxEnergized, len(energized))
-		clear(energized)
+		maxEnergized = max(maxEnergized, walkBeam(grid, Point{col, -1}, Point{0, 1}))
+		maxEnergized = max(maxEnergized, walkBeam(grid, Point{col, len(grid)}, Point{0, -1}))
 	}
 
 	var result2 = maxEnergized
 	fmt.Println("Day 16 Part 2 Result: ", result2)
 }
 
-type Whatever struct {
+type CheckPoint struct {
 	point, dir Point
 }
 
-func walkBeam(grid []string, pos, dir Point, energized map[Point]int) {
+func walkBeam(grid []string, pos, dir Point) int {
+	energized := make(map[Point]int, len(grid)*len(grid[0]))
 	testPoint := Point{pos.x + dir.x, pos.y + dir.y}
-	checkNext := []Whatever{{testPoint, dir}}
+	stillToCheck := []CheckPoint{{testPoint, dir}}
 
-	for len(checkNext) > 0 {
-		whatever := checkNext[0]
-		checkNext = checkNext[1:]
-		testPoint = whatever.point
-		dir = whatever.dir
+	for len(stillToCheck) > 0 {
+		checkNext := stillToCheck[0]
+		stillToCheck = stillToCheck[1:]
+		testPoint = checkNext.point
+		dir = checkNext.dir
 
 	mainLoop:
 		for insideGrid(grid, testPoint) && energized[testPoint] < 4 {
@@ -85,8 +73,8 @@ func walkBeam(grid []string, pos, dir Point, energized map[Point]int) {
 				case Point{0, 1}:
 					fallthrough
 				case Point{0, -1}:
-					checkNext = append(checkNext, Whatever{testPoint, Point{1, 0}})
-					checkNext = append(checkNext, Whatever{testPoint, Point{-1, 0}})
+					stillToCheck = append(stillToCheck, CheckPoint{testPoint, Point{1, 0}})
+					stillToCheck = append(stillToCheck, CheckPoint{testPoint, Point{-1, 0}})
 					break mainLoop
 				}
 			case '|':
@@ -94,14 +82,16 @@ func walkBeam(grid []string, pos, dir Point, energized map[Point]int) {
 				case Point{1, 0}:
 					fallthrough
 				case Point{-1, 0}:
-					checkNext = append(checkNext, Whatever{testPoint, Point{0, 1}})
-					checkNext = append(checkNext, Whatever{testPoint, Point{0, -1}})
+					stillToCheck = append(stillToCheck, CheckPoint{testPoint, Point{0, 1}})
+					stillToCheck = append(stillToCheck, CheckPoint{testPoint, Point{0, -1}})
 					break mainLoop
 				}
 			}
 			testPoint = Point{testPoint.x + dir.x, testPoint.y + dir.y}
 		}
 	}
+
+	return len(energized)
 }
 
 func insideGrid(grid []string, pos Point) bool {
