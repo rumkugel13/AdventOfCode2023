2a0e253fac8b52f0e5a0887fa52a57b79068dfac
rumkugel13 <rumkugel13@outlook.com>
2023-12-18 12:19:36 +0100

    add day18 part 2

--- day18.go
+++ day18.go
@@ -36,9 +36,43 @@ func day18() {
 	floodFill(trench, Point{1, 1})
 
 	var result = len(trench)
-	var result2 = 0
 	fmt.Println("Day 18 Part 1 Result: ", result)
+
+	nodes := make([]Point, 0, len(lines))
+	nodes = append(nodes, Point{0, 0})
+	point = Point{0, 0}
+	for _, line := range lines {
+		data := strings.Fields(line)
+		color := data[2][2 : len(data[2])-1]
+		hexNum := color[:5]
+		distance, _ := (strconv.ParseInt(hexNum, 16, 0))
+		dir := color[5]
+		switch dir {
+		case '0':
+			point.x += int(distance)
+		case '1':
+			point.y += int(distance)
+		case '2':
+			point.x -= int(distance)
+		case '3':
+			point.y -= int(distance)
+		}
+		nodes = append(nodes, point)
+	}
+
+	var result2 = findAreaShoelace(nodes) + 1
 	fmt.Println("Day 18 Part 2 Result: ", result2)
+}
+
+func findAreaShoelace(nodes []Point) int {
+	area := 0
+
+	for i := 0; i < len(nodes); i++ {
+		pointA, pointB := nodes[i], nodes[(i+1)%(len(nodes))]
+		area += (pointA.x * pointB.y) - (pointB.x * pointA.y) + max(abs(pointA.x-pointB.x), abs(pointA.y-pointB.y))
+	}
+
+	return area / 2
 }
 
 func floodFill(trench map[Point]string, point Point) {
