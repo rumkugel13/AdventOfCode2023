0c7ba476b53a2729f6d54f0d4d1b3e0a30e637fc
rumkugel13 <rumkugel13@outlook.com>
2023-12-19 14:06:13 +0100

    main: print elapsed time for each day

--- main.go
+++ main.go
@@ -2,27 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	fmt.Println("AoC 2023")
-	day01()
-	day02()
-	day03()
-	day04()
-	day05()
-	day06()
-	day07()
-	day08()
-	day09()
-	day10()
-	day11()
-	day12()
-	day13()
-	day14()
-	day15()
-	day16()
-	day17()
-	day18()
-	day19()
+
+	funcs := []func(){day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13, day14, day15, day16, day17, day18, day19}
+
+	for _, f := range funcs {
+		then := time.Now()
+		f()
+		fmt.Println("Took", time.Since(then))
+	}
 }
