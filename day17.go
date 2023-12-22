c809316ae616697e214d5c22a1bc18f008c3f0e0
rumkugel13 <rumkugel13@outlook.com>
2023-12-22 23:56:18 +0100

    day17: unrolled loop for ~100ms speedup

--- day17.go
+++ day17.go
@@ -35,24 +35,32 @@ func countHeatLoss(grid []string, start, end Point, minStreak, maxStreak int) in
 			minHeatLoss = min(minHeatLoss, visited[current])
 		}
 
-		for _, dir := range [3]Point{current.dir, dirLeft(current.dir), dirRight(current.dir)} {
-			nextPoint := Point{current.point.x + dir.x, current.point.y + dir.y}
-			if !insideGrid(grid, nextPoint) {
-				continue
+		straightState := HeatState{Point{current.point.x + current.dir.x, current.point.y + current.dir.y}, current.dir, current.streak + 1}
+		if insideGrid(grid, straightState.point) && current.streak < maxStreak {
+			totalHeatLoss := visited[current] + int(grid[straightState.point.y][straightState.point.x] - '0')
+			if val, found := visited[straightState]; !found || val > totalHeatLoss {
+				visited[straightState] = totalHeatLoss
+				pointsToCheck = append(pointsToCheck, straightState)
 			}
+		}
 
-			totalHeatLoss := visited[current] + int(grid[nextPoint.y][nextPoint.x] - '0')
-			nextStreak := 1
-			if dir == current.dir {
-				nextStreak = current.streak + 1
+		leftDir := dirLeft(current.dir)
+		leftState := HeatState{Point{current.point.x + leftDir.x, current.point.y + leftDir.y}, leftDir, 1}
+		if insideGrid(grid, leftState.point) && current.streak >= minStreak {
+			totalHeatLoss := visited[current] + int(grid[leftState.point.y][leftState.point.x] - '0')
+			if val, found := visited[leftState]; !found || val > totalHeatLoss {
+				visited[leftState] = totalHeatLoss
+				pointsToCheck = append(pointsToCheck, leftState)
 			}
-			if (dir == current.dir && current.streak < maxStreak) ||
-				(dir != current.dir && current.streak >= minStreak) {
-				nextState := HeatState{nextPoint, dir, nextStreak}
-				if val, found := visited[nextState]; !found || val > totalHeatLoss {
-					visited[nextState] = totalHeatLoss
-					pointsToCheck = append(pointsToCheck, nextState)
-				}
+		}
+
+		rightDir := dirRight(current.dir)
+		rightState := HeatState{Point{current.point.x + rightDir.x, current.point.y + rightDir.y}, rightDir, 1}
+		if insideGrid(grid, rightState.point) && current.streak >= minStreak {
+			totalHeatLoss := visited[current] + int(grid[rightState.point.y][rightState.point.x] - '0')
+			if val, found := visited[rightState]; !found || val > totalHeatLoss {
+				visited[rightState] = totalHeatLoss
+				pointsToCheck = append(pointsToCheck, rightState)
 			}
 		}
 	}
