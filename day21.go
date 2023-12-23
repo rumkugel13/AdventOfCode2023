24a0722fb313c723febc59cf8b9fd15e5808412b
rumkugel13 <rumkugel13@outlook.com>
2023-12-23 20:42:15 +0100

    day21: refactor part 2, now much faster

--- day21.go
+++ day21.go
@@ -10,45 +10,59 @@ func day21() {
 	startingPoint := findInGrid(grid, 'S')
 	visited := map[Point]bool{startingPoint: true}
 	for i := 0; i < 64; i++ {
-		visited = oneStep(grid, visited, false)
+		visited = oneStep(grid, visited)
 	}
 
 	var result = len(visited)
 	fmt.Println("Day 21 Part 1 Result: ", result)
 
-	visited2 := map[Point]bool{startingPoint: true}
-	yValues := [3]int{}
+	queue := []Point{startingPoint}
+	visitedOdd, visitedEven := map[Point]bool{}, map[Point]bool{startingPoint: true}
+	count := make([]int, (len(grid)/2)+2*len(grid)+1)
 	for i := 1; i <= (len(grid)/2)+2*len(grid); i++ {
-		visited2 = oneStep(grid, visited2, true)
-		if i == (len(grid) / 2) {
-			yValues[0] = len(visited2)
-		} else if i == (len(grid)/2)+len(grid) {
-			yValues[1] = len(visited2)
-		} else if i == (len(grid)/2)+2*len(grid) {
-			yValues[2] = len(visited2)
+		var currentVisited *map[Point]bool
+		if i%2 == 0 {
+			currentVisited = &visitedEven
+		} else {
+			currentVisited = &visitedOdd
 		}
+
+		queueLen := len(queue)
+		for i := 0; i < queueLen; i++ {
+			point := queue[i]
+			for _, dir := range [4]Point{{1, 0}, {-1, 0}, {0, 1}, {0, -1}} {
+				nextLocation := Point{point.x + dir.x, point.y + dir.y}
+				if grid[mod(nextLocation.y, len(grid))][mod(nextLocation.x, len(grid[0]))] != '#' {
+					if _, found := (*currentVisited)[nextLocation]; !found {
+						(*currentVisited)[nextLocation] = true
+						queue = append(queue, nextLocation)
+					}
+				}
+			}
+		}
+		queue = queue[queueLen:]
+		count[i] = len(*currentVisited)
 	}
 
-	var result2 = simplifiedLagrange(yValues[:])
+	var result2 = simplifiedLagrange(count[len(grid)/2], count[(len(grid)/2)+len(grid)], count[(len(grid)/2)+2*len(grid)])
 	fmt.Println("Day 21 Part 2 Result: ", result2)
 }
 
-func simplifiedLagrange(values []int) int {
-	a := values[0]/2 - values[1] + values[2]/2
-	b := -3*(values[0]/2) + 2*values[1] - values[2]/2
-	c := values[0]
+func simplifiedLagrange(v0, v1, v2 int) int {
+	a := v0/2 - v1 + v2/2
+	b := -3*(v0/2) + 2*v1 - v2/2
+	c := v0
 	x := 26501365 / 131
 	return a*x*x + b*x + c
 }
 
-func oneStep(grid []string, visitable map[Point]bool, part2 bool) map[Point]bool {
+func oneStep(grid []string, visitable map[Point]bool) map[Point]bool {
 	newVisitable := map[Point]bool{}
 
 	for point := range visitable {
 		for _, dir := range [4]Point{{1, 0}, {-1, 0}, {0, 1}, {0, -1}} {
 			nextLocation := Point{point.x + dir.x, point.y + dir.y}
-			if (part2 && grid[mod(nextLocation.y, len(grid))][mod(nextLocation.x, len(grid[0]))] != '#') ||
-				(!part2 && insideGrid(grid, nextLocation) && grid[nextLocation.y][nextLocation.x] != '#') {
+			if insideGrid(grid, nextLocation) && grid[nextLocation.y][nextLocation.x] != '#' {
 				newVisitable[nextLocation] = true
 			}
 		}
